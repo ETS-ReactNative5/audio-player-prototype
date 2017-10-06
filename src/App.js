12dfa36c68370d2bd062af022301d57d7ee26e2b
Yurick <yurick@flama.me>
2017-10-06 16:38:58 -0300

    Make svgs animate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import ReactSVG from 'react-svg'
 
+import AnimatedSVG from './AnimatedSVG'
 import MediaPlayer from './MediaPlayer'
 import leftBubbles from './left-bubbles.svg'
 import rightBubbles from './right-bubbles.svg'
@@ -8,13 +8,17 @@ import rightBubbles from './right-bubbles.svg'
 import './App.css'
 
 class App extends Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
         <section>
-          <ReactSVG path={leftBubbles} />
+          <AnimatedSVG path={leftBubbles} />
           <MediaPlayer src={['https://s3-sa-east-1.amazonaws.com/trapmp3/Episode141.mp3']} />
-          <ReactSVG path={rightBubbles} />
+          <AnimatedSVG path={rightBubbles} />
         </section>
       </div>
     )
